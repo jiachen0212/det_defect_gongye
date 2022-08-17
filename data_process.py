af66529fa6ef3fdd2fd395f6618f4decf1590d0c
陈佳 <chenjia@chenjiadeMacBook-Pro.local>
2022-08-17 17:19:15 +0800

    data process

--- data_process.py
+++ data_process.py
@@ -1,80 +1,84 @@
 # coding=utf-8
-import os
-import numpy as np 
 import cv2
-import matplotlib.pyplot as plt
+import os 
+import json 
+
+def mkdirs(path):
+    if not os.path.existis(path):
+        os.makedirs(path)
 
 
-def read_label():
-    data_path = r'D:\work\project\DL\kesen\data\dataset\dataset\train\label'
-    datas = [os.path.join(data_path, a) for a in os.listdir(data_path) if '.txt' in a]
-    defect_lines = []
-    for data_ in datas:
-        defect_lines += open(data_, 'r').readlines()
-
-    cls1, cls2, cls3 = 0, 0, 0
-    for line in defect_lines:
-        if line[0] == '1':
-            cls1 += 1
-        elif line[0] == '2':
-            cls2 += 1
-        elif line[0] == '3':
-            cls3 += 1
-    text = 'dianhen: {}, huahen: {}, zangzi: {}'.format(cls1, cls2, cls3)
-    print(text)
-
-
-def index_times(xs, ys):
-    # 取第一条样本: event中记录各个灰度值, xs, ys中分别是像素坐标(有重复记录)
+def Erode_Dilate(img):
+    # img = cv2.imread(img_path)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+    dst_Otsu1 = cv2.erode(img, kernel, iterations=1)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+    dst_Otsu2 = cv2.dilate(dst_Otsu1, kernel, 2)  
+    # cv2.imwrite('./auged/{}'.format(os.path.basename(img_path)), dst_Otsu1)
     
-    index_times = np.zeros((800, 1000))
-    for i in range(xs.shape[0]):
-        if xs[i] >= 700:
-            xs[i] -= 700
-        index_times[ys[i]][xs[i]] += 1
-
-    # r, c = np.where(index_times == np.max(index_times))
-    # print(r, c)
-    # print(index_times[207][33])
-    # cv2.circle(index_times, (c[0], r[0]), 10, (255,255,0), 0)
-    # cv2.circle(index_times, (c[1], r[1]), 10, (255,255,0), 0)
-    # cv2.circle(index_times, (c[2], r[2]), 10, (255,255,0), 0)
-    # cv2.imwrite('./index_times.bmp', index_times)
-
-    return index_times
+    return dst_Otsu2
 
 
-def index_values(value, xs, ys):
-    index_value = np.zeros((800, 1000))
-    for i in range(xs.shape[0]):
-        if xs[i] >= 700:
-            xs[i] -= 700
-        index_value[ys[i]][xs[i]] += value[i]/16
+def locate_circle(auged):
+    gray_auged = auged[:,:,0]
+    try:
+        circles = cv2.HoughCircles(gray_auged,cv2.HOUGH_GRADIENT,1,100,param1=60,param2=50,minRadius=320,maxRadius=400)[0]
+    except:
+        return None, None, 0
+    for circle in circles:
+        circle = [int(a) for a in circle]
+        cv2.circle(auged,(circle[0],circle[1]),circle[2],(0,255,0),2)
+
+    return auged, circles, 1
+
+
+def video2frames_and_aug2detcircle(video_path, out_dir_path, train_frames):
+    frame_circle = dict()
+    videos = os.listdir(video_path)
+    for video in videos:
+        # out_path = os.path.join(out_dir_path, video[:-4])
+        # mkdirs(out_path)
+        times = 0
+        frame_frequency = 1 
+        full_video_path = os.path.join(video_path, video)
+        print(full_video_path)
+        camera = cv2.VideoCapture(full_video_path)
+        while True:
+            times = times + 1
+            res, image = camera.read()
+            if res:
+                if times % frame_frequency == 0:
+                    auged_img = Erode_Dilate(image)
+                    # 提取aug后图像的圆心
+                    circled_img, circles, tag = locate_circle(auged_img)
+                    if tag != 0:
+                        auged_base_name = "{}_{}.jpg".format(video[:-4], times)
+                        save_im_name = os.path.join(out_dir_path, auged_base_name)
+                        train_im_name = os.path.join(train_frames, auged_base_name)
+                        # 保存提取到了圆心的图像帧, 且做画圆可视化
+                        cv2.imwrite(save_im_name, circled_img)
+                        # 提取到原因的帧, 保存未经处理的原图(后续可做一些别的aug使得训练更easy..)
+                        cv2.imwrite(train_im_name, image)
+                        if auged_base_name not in frame_circle:
+                            frame_circle[auged_base_name] = []
+                        frame_circle[auged_base_name].extend(circles)
+            else:
+                print('read video fail')
+                break 
+        camera.release()
+
+    return frame_circle
+
+
+if __name__ == "__main__":
+
+    # 1. 提取各视频的帧s
+    # 2. 形态学腐蚀+膨胀, aug下每一帧, 再用霍夫圆检测, 得到圆心.
+    # 3. 根据圆心和label.txt, 在未aug的原视频帧中定位box
+    video_path = '/Users/chenjia/Downloads/Smartmore/2022/比赛-工业表面缺陷检测/mp4s'
+    out_dir_path = '/Users/chenjia/Downloads/Smartmore/2022/比赛-工业表面缺陷检测/auged_frames'
+    train_frames = '/Users/chenjia/Downloads/Smartmore/2022/比赛-工业表面缺陷检测/train_frames'
+    frame_circle = video2frames_and_aug2detcircle(video_path, out_dir_path, train_frames)
+    with open("./video_frames_circle.json", "w") as f:
+        f.write(json.dumps(frame_circle, indent=4))
     
-    return index_value
-    
-
-def read_data(data_):
-    file = h5py.File(data_,"r") 
-    event_gs = np.array(file['events']['event_gs'])
-    t = np.array(file['events']['ts'])
-    x = np.array(file['events']['xs'])
-    y = np.array(file['events']['ys'])
-
-    return event_gs, t, x, y
-
-
-import h5py
-data_path = r'D:\work\project\DL\kesen\data\dataset\dataset\train\data'
-datas = [os.path.join(data_path, a) for a in os.listdir(data_path)]
-for data in datas:
-    basename = os.path.basename(data) # 1.h5
-    # print(basename[:-2]+'jpg')
-    event_gs, t, x, y = read_data(data)
-    image = np.zeros([1300, 800])
-    for i in range(1000000, 1500000):
-        image[x[i], y[i]] += int(event_gs[i]//8)
-        image[x[i], y[i]] = min(image[x[i], y[i]], 255) 
-    # cv2.imshow('', image)
-    # cv2.waitKey(1000)
-    # plt.show(image)
---
+++ pre_data_process.py
@@ -0,0 +1,52 @@
+# coding=utf-8
+import cv2
+import os
+import h5py
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm.auto import trange
+from BM3D import bm3d
+
+def hd52img_list(f, step):
+    img_list = []
+    event_g = np.array(f['events']['event_gs'])
+    t = np.array(f['events']['ts'])
+    x = np.array(f['events']['xs'])
+    y = np.array(f['events']['ys'])
+    for i in trange(math.ceil(len(event_g)/step)):
+        start = i * step
+        end = min(start + step, len(event_g))
+        img = np.zeros([1280, 800])
+        for p in range(start, end):
+            img[x[p], y[p]] += int(event_g[p] / 16)
+            img[x[p], y[p]] = min(img[x[p], y[p]], 255)
+        img_list.append(img)
+    
+    return img_list
+
+
+if __name__ == "__main__":
+
+    #1. generate video
+    # data_path = './dataset/train/data'
+    # step = 100000
+    # hd5s = [os.path.join(data_path, a) for a in os.listdir(data_path)]
+    # for hd5 in hd5s:
+    #     base_name = os.path.basename(hd5)
+    #     f = h5py.File(hd5, 'r')
+    #     img_list = hd52img_list(f, step)
+    #     video = cv2.VideoWriter("./{}.mp4".format(base_name[:-3]), cv2.VideoWriter_fourcc('m','p','4','v'), 10, (1280, 800))
+    #     for img in img_list:
+    #         img = cv2.cvtColor(img.astype(np.uint8).transpose(), cv2.COLOR_GRAY2BGR)
+    #         video.write(img)
+    #     video.release()
+
+    # 拆分video, 每一帧做噪声剔除
+    for i in range(1, 27):
+        im_name = './{}.jpg'.format(i)
+        bm3d(im_name)
+        
+
+
+
